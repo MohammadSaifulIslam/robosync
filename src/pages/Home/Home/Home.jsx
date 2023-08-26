d01c7e36eb0611c15554e1c42ae30b4a8b9d469b
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-08-26 07:17:55 -0700

    services section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 
 const Home = () => {
@@ -7,6 +8,8 @@ const Home = () => {
         <div>
             <Banner/>
             <AboutUs/>
+            <Services/>
+            
         </div>
     );
 };
